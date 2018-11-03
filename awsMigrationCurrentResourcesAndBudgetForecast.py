2caf00bd3ffca72af62bba1efc44f729da98dc6d
James <jameshamiltonwork887@gmail.com>
2018-11-03 00:22:25 -0500

    level 1 complete

--- awsMigrationCurrentResourcesAndBudgetForecast.py
+++ awsMigrationCurrentResourcesAndBudgetForecast.py
@@ -3,6 +3,8 @@ import pandas
 import urllib.request
 from pathlib import Path
 import time
+import numpy
+
 
 def pullExcelFromGithub():
     url = ('https://raw.githubusercontent.com/JamesHamiltonDev/craftDemo/master/hardware.xlsx')
@@ -10,7 +12,7 @@ def pullExcelFromGithub():
     fileNotFound = True
     while fileNotFound is True:
          print("Downloading file . . .")
-         time.sleep(5)
+         time.sleep(3)
          try:
              if pathToExcelFile.is_file():
                  fileNotFound = False
@@ -21,42 +23,67 @@ def pullExcelFromGithub():
          except:
              pass
 
+
 def sanitizeDataframe(hardwareDataframe):
     unsanitizedDataframe = hardwareDataframe
-#    print(unsanitizedDataframe)
-    sanitizeDataframe = unsanitizedDataframe.where((pandas.notnull(unsanitizedDataframe)), None)
+    sanitizeDataframe = unsanitizedDataframe.apply(lambda x: x.astype(str).str.lower().str.strip())
+#    .str.replace(' ', ''))
     sanitizeDataframe.columns = map(str.upper, sanitizeDataframe.columns)
-#    sanitizeDataframe = sanitizeDataframe
-#    sanitizeDataframe = unsanitizedDataframe.apply(lambda x: x.astype(str).str.lower().str.strip().str.replace(' ', ''))
-
-#    sanitizeDataframe.columns = map(str.upper, sanitizeDataframe.columns)
-#    upperHeaders = lowercaseDataframe.
-    print(sanitizeDataframe)
-
+#    sanitizeDataframe = sanitizeDataframe.where((numpy.nan(sanitizeDataframe)), None)
+#    sanitizeDataframe = sanitizeDataframe.astype(str).replace('engineeringcanada', 'engineering canada')
+    sanitizeDataframe = sanitizeDataframe.astype(object).replace('nan', 'None')
+    sanitizeDataframe = sanitizeDataframe.loc[sanitizeDataframe['LOGICAL STATUS'] == "operational"]
+    sanitizeDataframe['CPU CORES'] = sanitizeDataframe['CPU CORES'].astype(int)
+    sanitizeDataframe['RAM (MB)'] = sanitizeDataframe['RAM (MB)'].astype(int)
+    return sanitizeDataframe
 
 
 def readExcelIntoDataframe():
     waitingForDownload = pullExcelFromGithub()
-    hardwareExcelToDataframe = pandas.read_excel(open('ExcelTest.xlsx', 'rb'))
-#    print(hardwareExcelToDataframe)
-    sanitizedData = sanitizeDataframe(hardwareExcelToDataframe)
+    if waitingForDownload is False:
+        hardwareExcelToDataframe = pandas.read_excel(open('ExcelTest.xlsx', 'rb'))
+        sanitizedData = sanitizeDataframe(hardwareExcelToDataframe)
+#        listDepartmentsWithHardwareHosted(sanitizedData)
+        resourcesByDepartment(sanitizedData)
+        resourcesByApplication(sanitizedData)
+        resourcesByDataCenter(sanitizedData)
+    else:
+        print('Download failed')
 
 
-#    successfullDownload = Path('ExcelTest.xlsx')
-#    if waitingForDownload is False:
-#        print('Parsing excel into dataframe')
-#        hardwareExcel = pandas.read_excel('ExcelTest.xlsx')
-#        print(hardwareExcel)
-#    else:
-#        while waitingForDownload is not False:
-#            print("waiting . . .")
-#            try:
-#                if successfullDownload.is_file():
-#                    print('Parsing excel into dataframe')
-#                    hardwareExcel = pandas.read_excel('ExcelTest.xlsx')
-#                    print(hardwareExcel)
-#            except:
-#                pass
+def listDepartmentsWithHardwareHosted(departmentsHH):
+    pass
+#    departmentHostingHardware = departmentsHH
+#    departmentHostingHardware = departmentHostingHardware.GROUP.unique()
+#    arrayDepartmentsToList = list(departmentHostingHardware)
+#    print(arrayDepartmentsToList)
+
+
+def resourcesByDepartment(departmentResources):
+    resourcesBeingUsedByDepartment = departmentResources
+#    resourcesBeingUsedByDepartment['CPU CORES'] = resourcesBeingUsedByDepartment['CPU CORES'].astype(int)
+#    resourcesBeingUsedByDepartment['RAM (MB)'] = resourcesBeingUsedByDepartment['RAM (MB)'].astype(int)
+#    resourcesBeingUsedByDepartment = resourcesBeingUsedByDepartment.loc[
+#        resourcesBeingUsedByDepartment['LOGICAL STATUS'] == "operational"]
+    sumDepartmentResources = resourcesBeingUsedByDepartment.groupby(['GROUP'])[["CPU CORES", "RAM (MB)"]].sum()
+    print(sumDepartmentResources)
+
+
+def resourcesByApplication(applicationResources):
+    applicationsDepartment = applicationResources
+    applicationsDepartment = applicationsDepartment.sort_values(['GROUP'])
+    appsGroupedByDepartment = applicationsDepartment.groupby(['GROUP', 'APPLICATION'])[["CPU CORES", "RAM (MB)"]].sum()
+    print(appsGroupedByDepartment)
+
+
+def resourcesByDataCenter(dataCenterResources):
+    resourcesDatacenter = dataCenterResources
+    resourcesDatacenterSorted = resourcesDatacenter.sort_values(['SITE'])
+    groupedResourcesBySite = resourcesDatacenterSorted.groupby(['SITE'])[["CPU CORES", "RAM (MB)"]].sum()
+    print(groupedResourcesBySite)
+
+
+
 
 
 if __name__ == '__main__':
