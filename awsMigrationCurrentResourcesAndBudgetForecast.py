7a23784751349e62a8ca6bd557ce711519d5aead
James <jameshamiltonwork887@gmail.com>
2018-11-05 04:02:48 -0500

    test

--- awsMigrationCurrentResourcesAndBudgetForecast.py
+++ awsMigrationCurrentResourcesAndBudgetForecast.py
@@ -312,7 +312,7 @@ def toGitHub():
     commit_message = 'test'
     repo.index.add(file_list)
     repo.index.commit(commit_message)
-    origin = repo.remote('origin')
+    origin = repo.remote('origin development')
     origin.push()
 
 
